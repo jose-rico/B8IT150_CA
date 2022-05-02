8d7edaad784510827227a9d79bf205c526bc1c5d
jose-rico <104775935+jose-rico@users.noreply.github.com>
2022-05-02 23:05:09 +0100

    fixing identation issues

--- app.py
+++ app.py
@@ -26,23 +26,23 @@ def add():
 
 @app.route("/delete") #Delete Student
 def delete():
- id = request.args.get('id')
- cur = mysql.connection.cursor() #create a connection to the SQL instance
- s='''DELETE FROM students WHERE studentID=%s;'''
- cur.execute(s,id)
- mysql.connection.commit()
- return '{"Result":"Success"}'
+  id = request.args.get('id')
+  cur = mysql.connection.cursor() #create a connection to the SQL instance
+  s='''DELETE FROM students WHERE studentID=%s;'''
+  cur.execute(s,id)
+  mysql.connection.commit()
+  return '{"Result":"Success"}'
  
  @app.route("/update") #Update Student
 def update():
- name = request.args.get('name')
- email = request.args.get('email')
- id = request.args.get('id')
- cur = mysql.connection.cursor() #create a connection to the SQL instance
- s='''UPDATE students SET studentName=%s, email=%s WHERE ID=%s;'''
- cur.execute(s, name, email, id)
- mysql.connection.commit()
- return '{"Result":"Success"}'
+  name = request.args.get('name')
+  email = request.args.get('email')
+  id = request.args.get('id')
+  cur = mysql.connection.cursor() #create a connection to the SQL instance
+  s='''UPDATE students SET studentName=%s, email=%s WHERE ID=%s;'''
+  cur.execute(s, name, email, id)
+  mysql.connection.commit()
+  return '{"Result":"Success"}'
 
 
 @app.route("/") #Default - Show Data
