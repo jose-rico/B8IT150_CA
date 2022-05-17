66892961f57b19f2b164d8fee289c4f983bc40cb
jose-rico <104775935+jose-rico@users.noreply.github.com>
2022-05-17 12:55:10 +0100

    Adding column 'groupset' to DB, formating pages to see this change 3

--- app.py
+++ app.py
@@ -10,17 +10,17 @@ CORS(app)
 # Change the HOST IP and Password to match your instance configurations
 app.config['MYSQL_USER'] = 'web'
 app.config['MYSQL_PASSWORD'] = 'webPass'
-app.config['MYSQL_DB'] = 'student'
+app.config['MYSQL_DB'] = 'bike'
 app.config['MYSQL_HOST'] = 'localhost' #for now
 mysql.init_app(app)
 
 @app.route("/add") #Add Student
 def add():
-  name = request.args.get('name')
-  email = request.args.get('email')
+  bike = request.args.get('bike')
+  wheels = request.args.get('wheels')
   groupset = request.args.get('groupset')
   cur = mysql.connection.cursor() #create a connection to the SQL instance
-  s='''INSERT INTO students(studentName, email, groupset) VALUES('{}','{}','{}');'''.format(name,email,groupset)
+  s='''INSERT INTO bikes(bike, wheels, groupset) VALUES('{}','{}','{}');'''.format(bike,wheels,groupset)
   cur.execute(s)
   mysql.connection.commit()
   return '{"Result":"Success"}'
@@ -29,20 +29,20 @@ def add():
 def delete():
   id = request.args.get('id')
   cur = mysql.connection.cursor() #create a connection to the SQL instance
-  s='''DELETE FROM students WHERE studentID=%s;'''
+  s='''DELETE FROM bikes WHERE bikeID=%s;'''
   cur.execute(s,id)
   mysql.connection.commit()
   return '{"Result":"Success"}'
  
 @app.route("/update") #Update Student
 def update():
-  name = request.args.get('name')
-  email = request.args.get('email')
+  bike = request.args.get('bike')
+  wheels = request.args.get('wheels')
   groupset = request.args.get('groupset')
   id = request.args.get('id')
   cur = mysql.connection.cursor() #create a connection to the SQL instance
-  s='''UPDATE students SET studentName=%s, email=%s, groupset=%s WHERE studentID=%s;'''
-  cur.execute(s, (name, email, groupset, id))
+  s='''UPDATE bikes SET bike=%s, wheels=%s, groupset=%s WHERE bikeID=%s;'''
+  cur.execute(s, (bike, wheels, groupset, id))
   mysql.connection.commit()
   return '{"Result":"Success"}'
 
@@ -50,14 +50,14 @@ def update():
 @app.route("/") #Default - Show Data
 def hello(): # Name of the method
   cur = mysql.connection.cursor() #create a connection to the SQL instance
-  cur.execute('''SELECT * FROM students''') # execute an SQL statment
+  cur.execute('''SELECT * FROM bikes''') # execute an SQL statment
   rv = cur.fetchall() #Retreive all rows returend by the SQL statment
   Results=[]
   for row in rv: #Format the Output Results and add to return string
     Result={}
     print(row)
-    Result['Name']=row[0]#.replace('\n',' ') by disbling this it will handle records with no name add some javascritpt not allowing to insert null values!!!
-    Result['Email']=row[1]
+    Result['Bike']=row[0]#.replace('\n',' ') by disbling this it will handle records with no name add some javascritpt not allowing to insert null values!!!
+    Result['Wheels']=row[1]
     Result['Groupset']=row[2]
     Result['ID']=row[3]
     Results.append(Result)
