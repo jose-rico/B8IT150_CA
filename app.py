96f3b0cd9fd83a759f1d18748292860d1581f08b
jose-rico <104775935+jose-rico@users.noreply.github.com>
2022-05-26 19:41:27 +0100

    Modifiying app.py

--- app.py
+++ app.py
@@ -52,7 +52,26 @@ def update():
   mysql.connection.commit()
   return '{"Result":"Success"}'
 
-
+@app.route("/") #Default - Show Data
+def hello(): # Name of the method
+  cur = mysql.connection.cursor() #create a connection to the SQL instance
+  cur.execute('''SELECT * FROM customers''') # execute an SQL statment
+  rv = cur.fetchall() #Retreive all rows returend by the SQL statment
+  Results=[]
+  for row in rv: #Format the Output Results and add to return string
+    Result={}
+    print(row)
+    Result['Customer']=row[0]#.replace('\n',' ') by disbling this it will handle records with no name add some javascritpt not allowing to insert null values!!!
+    Result['Email']=row[1]
+    Result['ID']=row[2]
+    Results.append(Result)
+  response={'Results':Results, 'count':len(Results)}
+  ret=app.response_class(
+    response=json.dumps(response),
+    status=200,
+    mimetype='application/json'
+  )
+  return ret
 
 
 
