54396bfc01064081c09f6c5d2eb34abd9d712d9e
jose-rico <104775935+jose-rico@users.noreply.github.com>
2022-05-26 20:17:50 +0100

    Changing port to script app1.py

--- app.py
+++ app.py
@@ -52,32 +52,6 @@ def update():
   mysql.connection.commit()
   return '{"Result":"Success"}'
 
-@app.route("/") #Default - Show Data
-def hello(): # Name of the method
-  cur = mysql.connection.cursor() #create a connection to the SQL instance
-  cur.execute('''SELECT * FROM customers''') # execute an SQL statment
-  rv = cur.fetchall() #Retreive all rows returend by the SQL statment
-  Results=[]
-  for row in rv: #Format the Output Results and add to return string
-    Result={}
-    print(row)
-    Result['Customer']=row[0]#.replace('\n',' ') by disbling this it will handle records with no name add some javascritpt not allowing to insert null values!!!
-    Result['Email']=row[1]
-    Result['ID']=row[2]
-    Results.append(Result)
-  response={'Results':Results, 'count':len(Results)}
-  ret=app.response_class(
-    response=json.dumps(response),
-    status=200,
-    mimetype='application/json'
-  )
-  return ret
-if __name__ == "__main__":
-  app.run(host='0.0.0.0',port='8080', ssl_context=('/home/jose/cert.pem', '/home/jose/privkey.pem')) #Run the flask app at port 8080
-
-
-
-
 
 @app.route("/") #Default - Show Data
 def hello(): # Name of the method
